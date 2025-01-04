3e5bcfa8a96f88e16757cfcbacc3f0f90457c467
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-01-04 11:30:09 +0530

    adds execute expression

--- src/vinu.js
+++ src/vinu.js
@@ -1,8 +1,8 @@
-const add = (first, second) => first + second;
-const sub = (first, second) => first - second;
-const mul = (first, second) => first * second;
-const div = (first, second) => first / second;
-const mod = (first, second) => first % second;
+export const add = (first, second) => first + second;
+export const sub = (first, second) => first - second;
+export const mul = (first, second) => first * second;
+export const div = (first, second) => first / second;
+export const mod = (first, second) => first % second;
 
 const vinu_script = `Vinu 2.O
 exit using ctrl+d, ctrl+c, or close()
@@ -18,16 +18,18 @@ export const extract_operators = (expression) => expression.match(/[+-/*%]/g);
 export const is_expression_valid = (expression) =>
   !!expression.match(validate_expression);
 
+export const execute_expression = (operations, operands, operators) =>
+  operands.reduce((accumulator, operand, index) =>
+    operations[operators[index - 1]](accumulator, operand)
+  );
+
 export const evaluate = function (expression) {
   if (!is_expression_valid(expression)) return "parse error: Unexpected eof";
 
   const operations = { "+": add, "-": sub, "*": mul, "/": div, "%": mod };
   const operands = extract_operands(expression).map((number) => +number);
   const operators = extract_operators(expression);
-
-  return operands.reduce((accumulator, operand, index) =>
-    operations[operators[index - 1]](accumulator, operand)
-  );
+  return execute_expression(operations, operands, operators);
 };
 
 const main = function () {
