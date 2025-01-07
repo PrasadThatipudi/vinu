ef53c24d065c6a19610e2c2b70498737708d14da
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-01-07 22:16:31 +0530

    makes createVariable as pure function

--- src/evaluate.js
+++ src/evaluate.js
@@ -1,6 +1,13 @@
 import { replaceVariablesWithValues } from "./extract_variables.js";
 // non-capturing -> ?:
-const VALIDATE_EXPRESSION = /^\s*\d+\s*(\s*[+-/*%]\s*\d+)*$|^\s*$/;
+const add = (first, second) => first + second;
+const sub = (first, second) => first - second;
+const mul = (first, second) => first * second;
+const div = (first, second) => first / second;
+const mod = (first, second) => first % second;
+export const math = { add, sub, mul, div, mod };
+
+const VALIDATE_EXPRESSION = /^\s*\d+\s*(\s*[+-/*%]\s*\d+)*$/;
 export const extractOperands = (expression) => expression.match(/\d+/g);
 
 export const OPERATORS_REGEX = /[\+\-\/\*\%]/g;
@@ -28,18 +35,18 @@ export const executeExpression = (operations, operands, operators) =>
 
 export const parseErr = (err) => "\x1b[31mparse error\x1b[0m: " + err;
 
-export const evaluate = function (variables, expression) {
+const evaluate = (expression) => {
+  const operations = { "+": add, "-": sub, "*": mul, "/": div, "%": mod };
+  const operands = extractOperands(expression).map((number) => +number);
+  const operators = extractOperators(expression);
+  return executeExpression(operations, operands, operators);
+};
+
+export const evaluateExpression = function (variables, expression) {
   const exp = replaceVariablesWithValues(variables, expression);
   const [isErr, err] = validateVariables(exp);
 
   if (isErr) return `Uncaught ReferenceError: ` + err;
-
-  return eval(exp);
-
-  // if (!isExpressionValid(expression)) return parseErr("Unexpected eof");
-
-  // const operations = { "+": add, "-": sub, "*": mul, "/": div, "%": mod };
-  // const operands = extractOperands(expression).map((number) => +number);
-  // const operators = extractOperators(expression);
-  // return executeExpression(operations, operands, operators);
+  if (!isExpressionValid(exp)) return parseErr("Unexpected eof");
+  return evaluate(exp);
 };
--- src/vinu.js
+++ src/vinu.js
@@ -1,39 +1,44 @@
-import { evaluate } from "./evaluate.js";
+import { evaluateExpression } from "./evaluate.js";
 import { parseErr } from "./evaluate.js";
 import { extractVariable } from "./extract_variables.js";
 
-const vinu_version = `Vinu 2.O
+const version = `Vinu 1.O
 exit using ctrl+d, ctrl+c, or close()
 \x1b[33mREPL is running with all permissions allowed.\x1b[0m
 To specify permissions, run \`vinu repl\` with allow flags.`;
 
-const displayVersion = () => console.log(vinu_version);
+const displayVersion = () => console.log(version);
 
 const isVariableNameValid = function (variable) {
   return !variable.match(/^\s*\d+/);
 };
 
-const createVariable = function (vars, variable, type, value) {
+const createVariable = function (variables, variable, type, value) {
   if (!isNaN(variable))
-    return parseErr(
-      `Unexpected token \`numeric literal (${variable}, ${variable})\`. Expected yield, an identifier`
-    );
+    return [
+      variables,
+      parseErr(
+        `Unexpected token \`numeric literal (${variable}, ${variable})\`. Expected yield, an identifier`
+      ),
+    ];
 
   if (!isVariableNameValid(variable))
-    return parseErr(`Identifier cannot follow number`);
+    return [variables, parseErr(`Identifier cannot follow number`)];
 
-  if (variable in vars && vars[variable].type !== type)
-    return `Uncaught SyntaxError: Identifier '${variable}' has already been declared`;
+  if (variable in variables && variables[variable].type !== type)
+    return [
+      variables,
+      `Uncaught SyntaxError: Identifier '${variable}' has already been declared`,
+    ];
 
-  vars[variable] = { type, value };
-  // const newVar = { type, value };
-  // const result = { ...vars, [variable]: newVar };
-  // return result;
+  const newVar = { type, value };
+  const result = { ...variables, [variable]: newVar };
+  return [result];
 };
 
 const executeStatement = function (statement, variables) {
   const { variable, expression, type } = extractVariable(statement);
-  const result = evaluate(variables, expression);
+  const result = evaluateExpression(variables, expression);
 
   if (type && variable) {
     return createVariable(variables, variable, type, result);
@@ -46,18 +51,18 @@ const displayResult = (result) => console.log(result);
 
 const main = function () {
   displayVersion();
-  const variables = {};
+  let variables = {};
   let statement = prompt("> ").trim();
 
   while (statement !== "close()") {
-    // let result = undefined;
-    // if (statement.startsWith("const") || statement.startsWith("let")) {
-    //   variables = executeStatement(statement, variables);
-    // } else {
-    //   result = executeStatement(statement, variables);
-    // }
-    const result = executeStatement(statement, variables);
-    displayResult(result);
+    if (statement.startsWith("const") || statement.startsWith("let")) {
+      const [newVariables, err] = executeStatement(statement, variables);
+      variables = newVariables;
+      displayResult(err);
+    } else {
+      const result = executeStatement(statement, variables);
+      displayResult(result);
+    }
     statement = prompt("> ").trim();
   }
 };
